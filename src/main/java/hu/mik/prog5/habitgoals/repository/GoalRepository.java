e99168ffe3df241ce6c684f3c076e86d71ee7cc5
Proksza Manó Xavér <mproksza@npsh.hu>
2022-01-14 11:35:25 +0100

    Cleaning out last project code

--- src/main/java/hu/mik/prog5/habitgoals/repository/GoalRepository.java
+++
@@ -1,7 +0,0 @@
-package hu.mik.prog5.habitgoals.repository;
-
-import hu.mik.prog5.habitgoals.entity.Goal;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface GoalRepository extends MongoRepository<Goal, String>{
-}
--- src/main/java/hu/mik/prog5/habitgoals/service/GoalService.java
+++
@@ -1,5 +0,0 @@
-package hu.mik.prog5.habitgoals.service;
-
-public interface GoalService extends CRUDService{
-    boolean isCompleted();
-}
