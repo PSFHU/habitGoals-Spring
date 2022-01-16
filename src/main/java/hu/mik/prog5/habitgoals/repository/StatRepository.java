f2d2afc783cac4d73f3c0b2565433318719f0b5e
Proksza Manó Xavér <mproksza@npsh.hu>
2022-01-16 21:48:08 +0100

    Commenting extra feature for dev

--- src/main/java/hu/mik/prog5/habitgoals/repository/StatRepository.java
+++ src/main/java/hu/mik/prog5/habitgoals/repository/StatRepository.java
@@ -11,5 +11,5 @@ import java.util.List;
 @Repository
 public interface StatRepository extends JpaRepository<Stat,Long> {
 
-    List<Progress> getLastDays(int days);
+//    List<Progress> getLastDays(int days);
 }
