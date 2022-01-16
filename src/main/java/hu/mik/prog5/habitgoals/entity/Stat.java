449362146656b14b5813cce82dd98d60880da1b9
Proksza Manó Xavér <mproksza@npsh.hu>
2022-01-16 21:49:14 +0100

    Commenting out user connection for dev

--- src/main/java/hu/mik/prog5/habitgoals/entity/Stat.java
+++ src/main/java/hu/mik/prog5/habitgoals/entity/Stat.java
@@ -1,6 +1,8 @@
 package hu.mik.prog5.habitgoals.entity;
 
+import hu.mik.prog5.habitgoals.dto.UserDTO;
 import hu.mik.prog5.habitgoals.entity.goal.Goal;
+import hu.mik.prog5.habitgoals.entity.user.User;
 import lombok.Data;
 
 import javax.persistence.*;
@@ -14,13 +16,18 @@ public class Stat {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private Long uid;
+//    @OneToMany
+//    @JoinColumn(name = "user_id")
+//    private UserDTO user;
+
     @ManyToOne
     @JoinColumn(name = "unit_type_id")
     private Unit unit;
+
     @ManyToOne
     @JoinColumn(name = "goal_id")
     private Goal goal;
+
     @ManyToMany
     @JoinTable(name = "progress_stat",joinColumns = @JoinColumn(name = "stat_id"),inverseJoinColumns = @JoinColumn(name = "progress_id"))
     private List<Progress> progressList;
