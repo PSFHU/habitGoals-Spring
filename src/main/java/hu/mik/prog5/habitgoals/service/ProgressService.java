2562bed729579d942a8bbbd2f3ec688c9d33b830
Proksza Manó Xavér <mproksza@npsh.hu>
2022-01-16 21:46:21 +0100

    Fixing ProgressService

--- src/main/java/hu/mik/prog5/habitgoals/service/ProgressService.java
+++ src/main/java/hu/mik/prog5/habitgoals/service/ProgressService.java
@@ -11,7 +11,7 @@ import java.util.List;
 @Service
 @RequiredArgsConstructor
 public class ProgressService implements CRUDService<Progress>{
-    private ProgressRepository progressRepository;
+    private final ProgressRepository progressRepository;
 
     @Override
     public List findAll() {
